74ecdb04567c7a7799d6233e18809bf88280d589
Débora Fernandes C <95572299+Deboraaluna@users.noreply.github.com>
2022-05-12 20:14:03 -0300

    Atividade 2 de Baruque
    CBBAtividadepratica2.java

---
+++ CBBAtividadepratica2.java
@@ -0,0 +1,9 @@
+class CBBAtividadePratica2 {
+public static void main(String[] args){
+  int num1 = 5;
+  int num2 = 8;
+  int num3 = 2;
+  int soma = num1+num2+num3;
+System.out.println("num1+num2+num3=" +soma);
+}
+}
