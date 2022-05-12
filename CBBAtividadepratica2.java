5d729c6393a308c2a335ab5209b753fc9a69884e
Débora Fernandes C <95572299+Deboraaluna@users.noreply.github.com>
2022-05-12 20:30:20 -0300

    update CBBAtividadepratica2.java
    inserindo o calculo média.

--- CBBAtividadepratica2.java
+++ CBBAtividadepratica2.java
@@ -5,5 +5,6 @@ public static void main(String[] args){
   int num3 = 2;
   int soma = num1+num2+num3;
 System.out.println("num1+num2+num3=" +soma);
+System.out.println("Resultado da média é: " + (num1+num2+num3) / 3);
 }
 }
